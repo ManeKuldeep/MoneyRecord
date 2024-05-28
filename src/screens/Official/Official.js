1944cc2ab88ba815b64b431b30d92a052e568bf2
kuldeepmane <kuldeep.mane@codeb.online>
2024-05-28 20:59:03 +0530

    sales list and purchase list added

--- src/screens/Official/Official.js
+++ src/screens/Official/Official.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import {Button, StyleSheet} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
-export default function OfficialMainScreen() {
+export default function OfficialMainScreen({navigation}) {
   return (
     <SafeAreaView style={styles.container}>
-      <Button title="Sales" onPress={() => navigation.navigate('Sales')} />
+      <Button
+        title="Sales"
+        onPress={() => navigation.navigate('SalesScreen')}
+      />
       <Button
         title="Perchase"
-        onPress={() => navigation.navigate('Purchase')}
+        onPress={() => navigation.navigate('PurchaseScreen')}
       />
       <Button
         title="Statement"
-        onPress={() => navigation.navigate('Statement')}
+        onPress={() => navigation.navigate('StatementScreen')}
       />
     </SafeAreaView>
   );
--- src/screens/Official/Sales.js
+++ src/screens/Official/Sales.js
@@ -1,10 +1,129 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default function SalesScreen() {
+  const data = [
+    {
+      date: '23 June 2024',
+      particular: 'to shree cement',
+      amount: 200000,
+      gst: [180, 180],
+      billNo: 'abcd1234',
+      netAmount: 210000,
+    },
+    {
+      date: '23 June 2024',
+      particular: 'to jay cement',
+      amount: 200000,
+      gst: [180, 180],
+      billNo: 'abcd1234',
+      netAmount: 210000,
+    },
+    {
+      date: '23 June 2024',
+      particular: 'to JNPA',
+      amount: 200000,
+      gst: [180, 180],
+      billNo: 'abcd1234',
+      netAmount: 210000,
+    },
+  ];
   return (
-    <View>
-      <Text>SalesScreen</Text>
+    <View style={styles.container}>
+      {data.map(element => {
+        return (
+          <View style={styles.card}>
+            <View style={styles.header}>
+              <Text style={styles.subtitle}>{element.particular}</Text>
+            </View>
+            <View style={styles.content}>
+              <View style={styles.inlineContent}>
+                <View>
+                  <Text style={styles.label}>Date:</Text>
+                </View>
+                <View>
+                  <Text> {element.date}</Text>
+                </View>
+              </View>
+              <View style={styles.inlineContent}>
+                <View>
+                  <Text style={styles.label}>Bill No:</Text>
+                </View>
+                <View>
+                  <Text> {element.billNo}</Text>
+                </View>
+              </View>
+            </View>
+            <View style={styles.content}>
+              <View style={styles.inlineContent}>
+                <View>
+                  <Text style={styles.label}>Amount:</Text>
+                </View>
+                <View>
+                  <Text> {element.amount}</Text>
+                </View>
+              </View>
+              <View style={styles.inlineContent}>
+                <View>
+                  <Text style={styles.label}>GST:</Text>
+                </View>
+                <View>
+                  <Text> {element.gst.join('/')}</Text>
+                </View>
+              </View>
+            </View>
+            <View style={styles.content}>
+              <View style={styles.inlineContent}>
+                <View>
+                  <Text style={styles.label}>Net Amount:</Text>
+                </View>
+                <View>
+                  <Text> {element.netAmount}</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+        );
+      })}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  card: {
+    backgroundColor: 'white',
+    borderRadius: 15,
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 6,
+    elevation: 14,
+    justifyContent: 'center',
+    padding: 5,
+    marginHorizontal: 10,
+    marginVertical: 10,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  subtitle: {
+    fontSize: 24,
+  },
+  content: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  inlineContent: {
+    flexDirection: 'row',
+  },
+  label: {
+    fontWeight: 'bold',
+  },
+});
